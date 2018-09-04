f00014a5ccb4f0cae01d21bece1396215e95e365
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-09-04 11:00:30 +0600

    Simple example of java bean class

---
+++ src/main/java/Test.java
@@ -0,0 +1,9 @@
+package main.java;
+
+public class Test {
+    public static void main(String[] args) {
+        Employee employee = new Employee();
+        employee.setName("Danik");
+        System.out.println(employee.getName());
+    }
+}
